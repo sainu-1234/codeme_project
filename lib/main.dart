8f2abf3d660d15d756ad837181d70b761983ea02
sainu-1234 <abhidheensainul@gmail.com>
2026-08-10 23:25:43 +0530

    splash screen created

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:codeme_project/features/view/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: SplashScreen());
+  }
+}
